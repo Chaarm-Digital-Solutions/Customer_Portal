7c3fe986780e0844057b223933a25c3296cb48ca
Jakub Slawecki <58791816+pertheok@users.noreply.github.com>
2024-10-07 16:13:40 +0100

    Update Italian translation

--- resources/lang/it/dictionary.php
+++ resources/lang/it/dictionary.php
@@ -1,33 +1,203 @@
 <?php
 
 return [
+    'api-token-manager' => [
+        'cancel' => 'Annulla',
+        'close' => 'Chiudi',
+        'create' => 'Crea',
+        'created' => 'Creato.',
+        'delete' => 'Elimina',
+        'delete_confirmation' => 'Sei sicuro di voler eliminare questo token API?',
+        'manage' => 'Gestisci i token API',
+        'manage_description' => 'Puoi eliminare uno dei tuoi token esistenti se non sono più necessari.',
+        'permissions' => 'Permessi',
+        'token' => 'Token API',
+        'token_delete' => 'Elimina token API',
+        'token_disclaimer' => 'Si prega di copiare il nuovo token API. Per la tua sicurezza, non verrà mostrato di nuovo.',
+        'token_name' => 'Nome del token',
+        'token_permissions' => 'Permessi del token API',
+        'last_used' => 'Ultimo utilizzo',
+        'save' => 'Salva',
+    ],
+
+    'bills' => [
+        'title' => 'Fatture',
+    ],
+
+    'confirm-password' => [
+        'confirm' => 'Conferma',
+        'disclaimer' => 'Questa è un’area sicura dell’applicazione. Si prega di confermare la propria password prima di continuare.',
+        'password' => 'Password',
+    ],
+
+    'confirms-password' => [
+        'cancel' => 'Annulla',
+        'confirm' => 'Conferma',
+        'content' => 'Per la tua sicurezza, si prega di confermare la propria password per continuare.',
+        'password' => 'Password',
+        'password_confirm' => 'Conferma password',
+    ],
+
+    'delete-user-form' => [
+        'cancel' => 'Annulla',
+        'content' => 'Una volta eliminato il tuo account, tutte le sue risorse e dati verranno eliminati in modo permanente. Prima di eliminare il tuo account, scarica eventuali dati o informazioni che desideri mantenere.',
+        'description' => 'Elimina permanentemente il tuo account.',
+        'disclaimer' => 'Sei sicuro di voler eliminare il tuo account? Una volta eliminato il tuo account, tutte le sue risorse e dati verranno eliminati in modo permanente. Si prega di inserire la propria password per confermare di voler eliminare permanentemente il proprio account.',
+        'password' => 'Password',
+    ],
+
+    'dashboard' => [
+        'title' => 'Cruscotto',
+    ],
+
+    'forgot-password' => [
+        'disclaimer' => 'Hai dimenticato la tua password? Nessun problema. Faccelo sapere il tuo indirizzo email e ti invieremo un link per il reset della password che ti permetterà di sceglierne una nuova.',
+        'email' => 'Email',
+        'send_link' => 'Invia link per reset della password via email',
+    ],
+
+    'index' => [
+        'api_tokens' => 'Token API',
+    ],
+
+    'logout-other-browser-sessions-form' => [
+        'cancel' => 'Annulla',
+        'content' => 'Se necessario, puoi disconnetterti da tutte le tue altre sessioni del browser su tutti i tuoi dispositivi. Alcune delle tue sessioni recenti sono elencate di seguito; tuttavia, questo elenco potrebbe non essere esaustivo. Se ritieni che il tuo account sia stato compromesso, dovresti anche aggiornare la tua password.',
+        'description' => 'Gestisci e disconnetti le tue sessioni attive su altri browser e dispositivi.',
+        'done' => 'Fatto.',
+        'last_active' => 'Ultima attività',
+        'log_out' => 'Disconnetti altre sessioni del browser',
+        'password' => 'Password',
+        'password_disclaimer' => 'Si prega di inserire la propria password per confermare di voler disconnettere le proprie altre sessioni del browser su tutti i propri dispositivi.',
+        'this_device' => 'Questo dispositivo',
+        'unknown' => 'Sconosciuto',
+    ],
+
     'login' => [
         'email' => 'Email',
+        'forgot_password' => 'Hai dimenticato la tua password?',
+        'login' => 'Accedi',
         'password' => 'Password',
         'remember_me' => 'Ricordami',
-        'forgot_password' => 'Hai dimenticato la password?',
-        'login' => 'Accedi',
     ],
+
+    'navigation-menu' => [
+        'api_tokens' => 'Token API',
+        'bills' => 'Fatture',
+        'create_new_team' => 'Crea un nuovo team',
+        'dashboard' => 'Cruscotto',
+        'log_out' => 'Disconnetti',
+        'manage_account' => 'Gestisci account',
+        'manage_team' => 'Gestisci team',
+        'meter_reads' => 'Letture del contatore',
+        'payments' => 'Pagamenti',
+        'profile' => 'Profilo',
+        'switch_teams' => 'Cambia team',
+        'team_settings' => 'Impostazioni team',
+    ],
+
+    'payments' => [
+        'title' => 'Pagamenti',
+    ],
+
+    'reads' => [
+        'title' => 'Letture del contatore',
+    ],
+
     'register' => [
-        'name' => 'Nome',
-        'email' => 'Email',
-        'password' => 'Password',
+        'already_registered' => 'Già registrato?',
         'confirm_password' => 'Conferma password',
-        'already_registered' => 'Sei già registrato?',
+        'disclaimer' => 'Accetto i :terms_of_service e la :privacy_policy',
+        'disclaimer_privacy' => 'Informativa sulla privacy',
+        'disclaimer_tos' => 'Termini di servizio',
+        'email' => 'Email',
+        'name' => 'Nome',
+        'password' => 'Password',
         'register' => 'Registrati',
     ],
-    'navigation-menu' => [
-        'dashboard' => 'Cruscotto',
-        'bills' => 'Fatture',
-        'payments' => 'Pagamenti',
-        'meter_reads' => 'Letture del contatore',
-        'manage_team' => 'Gestisci team',
-        'team_settings' => 'Impostazioni team',
-        'create_new_team' => 'Crea nuovo team',
-        'switch_teams' => 'Cambia team',
-        'manage_account' => 'Gestisci account',
+
+    'reset-password' => [
+        'confirm_password' => 'Conferma password',
+        'email' => 'Email',
+        'password' => 'Password',
+        'reset_password' => 'Reimposta password',
+    ],
+
+    'show' => [
         'profile' => 'Profilo',
-        'api_tokens' => 'Token API',
+    ],
+
+    'team-invitation' => [
+        'accept' => 'Accetta invito',
+        'create_account' => 'Crea account',
+        'discard' => 'Se non ti aspettavi di ricevere un invito a questo team, puoi eliminare questa email.',
+        'invitation' => 'Sei stato invitato a unirti al team :team!',
+        'invite_accept' => 'Puoi accettare questo invito facendo clic sul pulsante qui sotto:',
+        'register' => 'Se non hai un account, puoi crearne uno facendo clic sul pulsante qui sotto. Dopo aver creato un account, puoi fare clic sul pulsante di accettazione dell'invito in questa email per accettare l'invito al team:',
+        'register_accept' => 'Se hai già un account, puoi accettare questo invito facendo clic sul pulsante qui sotto:',
+    ],
+
+    'two-factor-authentication-form' => [
+        'cancel' => 'Annulla',
+        'code' => 'Codice',
+        'code_prompt' => 'Per completare l\'attivazione dell\'autenticazione a due fattori, scansiona il seguente codice QR utilizzando l\'app di autenticazione del tuo telefono oppure inserisci la chiave di configurazione e fornisci il codice OTP generato.',
+        'codes_disclaimer' => 'Conserva questi codici di recupero in un gestore di password sicuro. Possono essere utilizzati per recuperare l\'accesso al tuo account se il tuo dispositivo di autenticazione a due fattori viene perso.',
+        'confirm' => 'Conferma',
+        'disable' => 'Disabilita',
+        'elaboration' => 'Quando l\'autenticazione a due fattori è abilitata, ti verrà richiesto un token sicuro e casuale durante l\'autenticazione. Puoi recuperare questo token dall\'app Google Authenticator del tuo telefono.',
+        'enable' => 'Abilita',
+        'enable_confirmation' => 'Hai abilitato l\'autenticazione a due fattori.',
+        'finish_enabling' => 'Completa l\'attivazione dell\'autenticazione a due fattori.',
+        'not_enabled' => 'Non hai abilitato l\'autenticazione a due fattori.',
+        'regenerate' => 'Rigenera codici di recupero',
+        'setup_key' => 'Chiave di configurazione',
+        'show' => 'Mostra codici di recupero',
+    ],
+
+    'two-factor-challenge' => [
+        'authentication_description' => 'Si prega di confermare l\'accesso al proprio account inserendo il codice di autenticazione fornito dalla propria applicazione di autenticazione.',
+        'code' => 'Codice',
+        'log_in' => 'Accedi',
+        'recovery_code' => 'Codice di recupero',
+        'recovery_description' => 'Si prega di confermare l\'accesso al proprio account inserendo uno dei propri codici di emergenza per il recupero.',
+        'use_authentication' => 'Usa un codice di autenticazione',
+        'use_recovery' => 'Usa un codice di recupero',
+    ],
+
+    'update-password-form' => [
+        'cancel' => 'Annulla',
+        'confirm_password' => 'Conferma Password',
+        'current_password' => 'Password attuale',
+        'description' => 'Assicurati che il tuo account utilizzi una password lunga e casuale per rimanere al sicuro.',
+        'new_password' => 'Nuova Password',
+        'saved' => 'Salvato.',
+        'save' => 'Salva',
+    ],
+
+    'update-profile-information-form' => [
+        'cancel' => 'Annulla',
+        'description' => 'Aggiorna le informazioni del profilo e l\'indirizzo email del tuo account.',
+        'email' => 'Email',
+        'email_unverified' => 'Il tuo indirizzo email non è verificato.',
+        'email_verify' => 'Clicca qui per reinviare l\'email di verifica.',
+        'email_verify_sent' => 'Un nuovo link di verifica è stato inviato al tuo indirizzo email.',
+        'name' => 'Nome',
+        'new_photo' => 'Seleziona una nuova foto',
+        'photo' => 'Foto',
+        'remove_photo' => 'Rimuovi foto',
+        'saved' => 'Salvato',
+        'title' => 'Informazioni sul profilo',
+    ],
+
+    'validation-errors' => [
+        'error' => 'Ops! Qualcosa è andato storto.',
+    ],
+
+    'verify-email' => [
+        'description' => 'Prima di continuare, puoi verificare il tuo indirizzo email facendo clic sul link che ti abbiamo appena inviato via email? Se non hai ricevuto l\'email, saremo lieti di inviartene un\'altra.',
+        'edit_profile' => 'Modifica profilo',
+        'link_resend' => 'Reinvia email di verifica',
+        'link_sent' => 'Un nuovo link di verifica è stato inviato all\'indirizzo email fornito nelle impostazioni del tuo profilo.',
         'log_out' => 'Disconnetti',
     ],
 ];
