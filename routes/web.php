313e4a7fee426e2596f9c984ab36f00ec4ee30ca
Jakub Slawecki <58791816+pertheok@users.noreply.github.com>
2024-10-07 08:37:35 +0100

    Set the initial layout

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,16 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/bills', function () {
+        return view('bills');
+    })->name('bills');
+
+    Route::get('/payments', function () {
+        return view('payments');
+    })->name('payments');
+
+    Route::get('/reads', function () {
+        return view('reads');
+    })->name('reads');
 });
