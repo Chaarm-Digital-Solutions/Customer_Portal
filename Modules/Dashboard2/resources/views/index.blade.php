44cd29c5b68c88188fc55e2c07b625b50fb73e81
Jakub Slawecki <58791816+pertheok@users.noreply.github.com>
2024-12-11 11:42:22 +0000

    initialise the module files

---
+++ Modules/Dashboard2/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('dashboard2::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('dashboard2.name') !!}</p>
+@endsection
---
+++ Modules/Dashboard2/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Dashboard2\Http\Controllers\Dashboard2Controller;
+
+/*
+ *--------------------------------------------------------------------------
+ * API Routes
+ *--------------------------------------------------------------------------
+ *
+ * Here is where you can register API routes for your application. These
+ * routes are loaded by the RouteServiceProvider within a group which
+ * is assigned the "api" middleware group. Enjoy building your API!
+ *
+*/
+
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+    Route::apiResource('dashboard2', Dashboard2Controller::class)->names('dashboard2');
+});
