82f1a8882d14f11f7d9a9c73740530e9b552e153
Jakub Slawecki <58791816+pertheok@users.noreply.github.com>
2024-12-19 14:53:39 +0000

    Point towards the correct lang file

--- Modules/Dashboard2/resources/views/index.blade.php
+++ Modules/Dashboard2/resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard 2.0') }}
+            {{ __('dashboard2::dictionary.dashboard2.title') }}
         </h2>
     </x-slot>
 
