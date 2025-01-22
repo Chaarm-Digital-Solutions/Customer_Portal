5ce58b28306260c704d2548862c3152b6e93bd51
Jakub Slawecki <58791816+pertheok@users.noreply.github.com>
2025-01-22 13:02:22 +0000

    Initialise config saving

--- Modules/Dashboard2/resources/assets/js/app.js
+++ Modules/Dashboard2/resources/assets/js/app.js
@@ -19,3 +19,48 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+function removeWidget(button) {
+    var widgetElement = button.closest('.grid-stack-item');
+    var widget = dashboardGrid.getGridItems().find(item => item === widgetElement);
+    dashboardGrid.removeWidget(widget);
+}
+
+function saveGridConfig () {
+
+    // Select items present on the grid
+    var gridItems = dashboardGrid.engine.nodes;
+
+    // get specific atrributes of those items
+    var simplifiedData = gridItems.map(function (item) {
+        return {
+            x: item.x,
+            y: item.y,
+            w: item.w,
+            h: item.h,
+            widgetType: item.widgetType
+        };
+    });
+    var serializedData = JSON.stringify(simplifiedData);
+
+    // Send the serialized configuration to the server
+    $.ajax({
+        url: "{{ route('dashboard.save') }}",
+        type: 'POST',
+        data: { 
+                _token: '{{ csrf_token() }}',
+                user_id: userId,
+                user_config: serializedData 
+        },
+    });
+};
+
+// Whenever grid items is manipulated, save the grid state
+dashboardGrid.on('change', function(event, items) {
+    saveGridConfig();
+});
+
+// Save the grid config after an item has been removed
+dashboardGrid.on('removed', function(event, items) {
+    saveGridConfig();
+});
