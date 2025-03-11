fb5e2459084d2c0c0053d0248e891f0b08c131c9
Jakub Slawecki <58791816+pertheok@users.noreply.github.com>
2025-03-11 08:51:34 +0000

    make some columns nullable

--- Modules/GanttChart/database/migrations/2025_03_11_082452_create_tasks_table.php
+++ Modules/GanttChart/database/migrations/2025_03_11_082452_create_tasks_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('project');
+            $table->bigInteger('project')->nullable();
             $table->string('name');
             $table->date('start');
             $table->date('end');
             $table->tinyInteger('progress');
-            $table->string('dependencies');
+            $table->string('dependencies')->nullable();
             $table->timestamps();
         });
     }
