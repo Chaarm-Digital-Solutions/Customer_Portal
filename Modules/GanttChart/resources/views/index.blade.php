e6c100662b0a16b9ac9c025aa307010946f5f948
Jakub Slawecki <58791816+pertheok@users.noreply.github.com>
2025-03-11 14:46:34 +0000

    initialise forms

--- Modules/GanttChart/resources/views/index.blade.php
+++ Modules/GanttChart/resources/views/index.blade.php
@@ -15,41 +15,8 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <svg id="gantt"></svg>
             </div>
-            <div id="controls-container">
-                <form action="{{ route('task.create') }}" id="add-task" method="POST">
-                    @csrf
-                    <h2>Add task</h2>
-                    <label for="task-name">Task name</label>
-                    <input id="task-name" name="task-name" required />
-                    <label for="description">Description</label>
-                    <input type="text" name="task-description" id="task-description">
-                    <label for="start-date">Start date</label>
-                    <input
-                        type="date"
-                        id="start-date"
-                        name="start-date"
-                        required
-                    />
-                    <label for="end-date">End date</label>
-                    <input
-                        type="date"
-                        id="end-date"
-                        name="end-date"
-                        required
-                    />
-                    <label for="progress">Progress (%)</label>
-                    <input
-                        type="number"
-                        id="progress"
-                        name="progress"
-                        value="0"
-                        step="1"
-                        min="0"
-                        max="100"
-                        required
-                    />
-                    <input type="submit" value="add">
-                </form>
+            <div class="mt-4 bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-ganttchart::create-task-form />
             </div>
         </div>
     </div>
